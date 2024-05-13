35a02d874c32a72ef7eae162fdc0a1032cfc8168
wojtekbier03 <alina-biernacka1@wp.pl>
2024-05-13 16:56:08 +0200

    project

--- src/main/java/com/wojtekbier03/schoolsystem/Teacher/Repository/TeacherRepository.java
+++ src/main/java/com/wojtekbier03/schoolsystem/Teacher/Repository/TeacherRepository.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-
+//s
 @Repository
 public class TeacherRepository {
     private final List<Teacher> teachers = new ArrayList<>();
